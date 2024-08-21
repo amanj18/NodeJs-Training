0df5dd22a407ada16498f95b60bf0d1e51089053
Aman Jaiswal <89749348+amanj18@users.noreply.github.com>
2024-08-21 12:44:28 +0530

    Delete useESModule.mjs

--- useESModule.mjs
+++
@@ -1,7 +0,0 @@
-import message,{multiply,divide} from "./calcESModule.mjs";
-import simple from './calculator.js'
-console.log(message);
-console.log("Division " + divide(2,1))
-console.log("Multiply" + multiply(10,3))
-
-console.log("Addition" + simple.add(11,21))
