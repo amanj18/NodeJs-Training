d327ef9c2168c368fad6c5e287e2434c2bbf6675
amanj18 <amanjais160@gmail.com>
2024-08-21 11:49:01 +0530

    UPDATED

--- simple.js
+++ simple.js
@@ -1,29 +1,30 @@
 // const calc = require('./calculator')
 const {add,PI} = require('./calculator')
-console.log('welcome')
+// const calc = require('./calcESModule.mjs')
 
-// console.log(message)
+console.log("Welcome" );
+// console.log(calc.message)
 
-let addResult = add(5,6)
+let addResult = add(5,6);
 
 console.log("Add result" + addResult)
-let area = PI*3*3
+let area = PI * 3 * 3;
+
 console.log("Area" + area)
 
-//to import a ES6 module dynamically
-import ('./calcFSModule.mjs').then(
-    es=> {console.log(es.default);
-        console.log(es.multiple(10,10));
+// To import a ES6 module dynamically
+import ('./calcESModule.mjs').then(
+    es => {console.log(es.default);
+        console.log(es.multiply(10,10))
     }
 )
 
-
-
-
-async function divide () {
-    const es6Module = await import('./calcFSModule.mjs')
-    const result = await es6Module.divide(100, 5);
-    console.log("Divison result" + result);
+async function divide (){
+    const es6Module = await import('./calcESModule.mjs')
+    const result = await es6Module.divide(100,5)
+    console.log("Add result" + result)
 }
+
+
 
 divide();
